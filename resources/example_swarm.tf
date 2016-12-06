724a60dc9cc3de0b7114b3064a54f9ba931a3296
Ramazan Varlikli <rvarlikli@yahoo.com>
2016-12-06 12:51:47 +0300

    example tf files modified

--- resources/example_swarm.tf
+++ resources/example_swarm.tf
@@ -1,3 +1,7 @@
+provider "ciscodocker" {
+  host = "tcp://192.168.99.100:2376/"
+}
+
 resource "ciscodocker_swarm" "swarm" {
   listen_address = "0.0.0.0:2377"
   advertise_address = "192.168.99.100:2377"
--- resources/example_swarmnode.tf
+++ resources/example_swarmnode.tf
@@ -1,8 +1,15 @@
-resource "ciscodocker_swarmnode" "swarm-node" {
-  api_address = "192.168.99.101"
-  api_port = 2375
+provider "ciscodocker" {
+  alias = "node1"
+  host = "tcp://192.168.99.101:2376/"
+  cert_path = "/Users/kadirtaskiran/.docker/machine/machines/node1"
+}
+
+resource "ciscodocker_swarmnode" "node1" {
+  provider = "ciscodocker.node1"
   listen_address = "0.0.0.0:2377"
   advertise_address = "192.168.99.101:2377"
-  is_swarm_manager = true
-  swarm_manager_address = "192.168.99.100:2377"
+  is_manager = false
+  //manager_token = comes from swarm manager / string
+  //worker_token = comes from swarm manager / string
+  remote_address = ["192.168.99.100:2377"]
 }
