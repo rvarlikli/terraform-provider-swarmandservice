998b82e40927e01ec4385c0d789d14414be10f89
Ramazan Varlikli <rvarlikli@yahoo.com>
2016-12-28 13:58:19 +0300

    Added gitlab-ce and java services

--- test.tf
+++ test.tf
@@ -31,6 +31,69 @@ resource "ciscodocker_swarmnode" "node1" {
   remote_address = ["192.168.99.100:2377"]
 }
 
+resource "ciscodocker_service" "gitlab-ce" {
+  depends_on = ["ciscodocker_swarm.manager", "ciscodocker_swarmnode.node1"]
+  provider = "ciscodocker.manager"
+  service_name="gitlab"
+  image_name="gitlab/gitlab-ce"
+  restart_policy_condition = "any"
+  service_replica_count=1
+  env = ["GITLAB_OMNIBUS_CONFIG=\"external_url 'http://nesillocal.com:8088/'; gitlab_rails['gitlab_shell_ssh_port'] = 8922;\""]
+  ports = {
+    published_port = 8088
+    target_port = 80
+    protocol = "tcp"
+  }
+  ports = {
+    published_port = 8443
+    target_port = 443
+    protocol = "tcp"
+  }
+  ports = {
+    published_port = 8922
+    target_port = 22
+    protocol = "tcp"
+  }
+  resolution_mode = "vip"
+
+}
+
+resource "ciscodocker_service" "java-pie-app" {
+  depends_on = ["ciscodocker_swarm.manager", "ciscodocker_swarmnode.node1"]
+  provider = "ciscodocker.manager"
+  service_name="java-pie-app"
+  image_name="cloudnesil/openjdk:8u111-jdk"
+  restart_policy_condition = "any"
+  service_replica_count=1
+  ports = {
+    published_port = 8090
+    target_port = 8080
+    protocol = "tcp"
+  }
+  resolution_mode = "vip"
+  command = ["startapp.sh"]
+  args = ["master"]
+
+}
+
+resource "ciscodocker_service" "java-pie-appv2" {
+  depends_on = ["ciscodocker_swarm.manager", "ciscodocker_swarmnode.node1"]
+  provider = "ciscodocker.manager"
+  service_name="java-pie-appv2"
+  image_name="cloudnesil/openjdk:8u111-jdk"
+  restart_policy_condition = "any"
+  service_replica_count=1
+  ports = {
+    published_port = 8091
+    target_port = 8080
+    protocol = "tcp"
+  }
+  resolution_mode = "vip"
+  command = ["startapp.sh"]
+  args = ["v2"]
+
+}
+
 resource "ciscodocker_service" "hello" {
   depends_on = ["ciscodocker_swarm.manager", "ciscodocker_swarmnode.node1"]
   provider = "ciscodocker.manager"
